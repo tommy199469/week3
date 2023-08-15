3263c8be29d28efafb9c4f3978d959b0ead1332b
Ng KA HO <kngg0071@student.monash.edu>
2023-08-14 19:46:40 -0700

    Initial commit

---
+++ week3/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using week3.Models.HelloWorld;
+
+namespace week3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            Hello hello = new Hello();
+
+            ViewBag.Message = hello.GetHello();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
